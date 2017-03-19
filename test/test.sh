104c5586b8d22a667b30170ac1f1c19e473cdd6c
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-03-19 10:39:05 +0100

    fix ignore rules

--- test/test.sh
+++
@@ -1,9 +0,0 @@
-source testify.bash
-source ../todo.sh
-source functions.sh
-assert expect "$(addTodo)" "Incomplete requirement for adding todo" "Test for command line argument" "should pass"
-assert expect "$(addTodo $1)" "" "add todo" "should pass"
-assert expect "$(addTodo $1)" "$1 already exists, choose a new name" "if file already exists" "should pass"
-assert done
-
-
